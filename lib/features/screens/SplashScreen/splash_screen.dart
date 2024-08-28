a472ce5f0954bf07627e0dc34b1d7f04b3de27e1
anubhav-18 <anubhavbindal18@gmail.com>
2024-08-28 22:42:07 +0530

    loading added in splashscreen

--- lib/features/screens/SplashScreen/splash_screen.dart
+++ lib/features/screens/SplashScreen/splash_screen.dart
@@ -1,5 +1,6 @@
 import 'package:conquest/features/BottomNavBar/bottom_nav_bar.dart';
 import 'package:flutter/material.dart';
+import 'package:loading_indicator/loading_indicator.dart';
 import 'package:page_transition/page_transition.dart';
 
 class SplashScreen extends StatefulWidget {
@@ -15,7 +16,7 @@ class _SplashScreenState extends State<SplashScreen> {
     super.initState();
 
     // Delay for 3 seconds and then navigate to HomePage
-    Future.delayed(const Duration(seconds: 3), () {
+    Future.delayed(const Duration(seconds: 2), () {
       Navigator.pushAndRemoveUntil(
         context,
         PageTransition(
@@ -31,9 +32,25 @@ class _SplashScreenState extends State<SplashScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Image.asset(
-          'assets/logos/conquest-string-icon.png',
-          height: 200,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset(
+              'assets/logos/conquest-string-icon.png',
+              height: 200,
+            ),
+            SizedBox(
+              height: MediaQuery.of(context).size.height * 0.25,
+            ),
+            SizedBox(
+              height: MediaQuery.of(context).size.height * 0.06,
+              child: const LoadingIndicator(
+                indicatorType: Indicator.ballRotateChase,
+                colors: [Colors.red, Colors.redAccent],
+                strokeWidth: 2,
+              ),
+            ),
+          ],
         ),
       ),
     );
