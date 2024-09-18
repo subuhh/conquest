8d5f8655c9111c1101dc619cbc272a716ece24eb
shubham <subham13jan@gmail.com>
2024-09-19 00:23:07 +0530

    bottom nav bar updated

--- lib/features/BottomNavBar/animated_notch_bottom_bar/animated_notch_bottom_bar.dart
+++
@@ -1,6 +0,0 @@
-library animated_notch_bottom_bar;
-
-export 'package:conquest/features/BottomNavBar/src/models/bottom_bar_item_model.dart';
-export 'package:conquest/features/BottomNavBar/src/notch_bottom_bar.dart';
-export 'package:conquest/features/BottomNavBar/src/notch_bottom_bar_controller.dart';
-
--- lib/features/BottomNavBar/bottom_nav_bar.dart
+++ lib/features/BottomNavBar/bottom_nav_bar.dart
@@ -1,13 +1,10 @@
-import 'package:conquest/features/BottomNavBar/src/models/bottom_bar_item_model.dart';
-import 'package:conquest/features/BottomNavBar/src/notch_bottom_bar.dart';
-import 'package:conquest/features/BottomNavBar/src/notch_bottom_bar_controller.dart';
-
-import 'package:conquest/features/HomePage/homepage.dart';
-import 'package:conquest/features/MarketPlace/market_place_screen.dart';
+import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
-import '../../utils/constants/colors.dart';
+import '../../utils/constants/colors.dart'; // Make sure to import your constants
+import 'package:conquest/features/HomePage/homepage.dart';
+import 'package:conquest/features/MarketPlace/market_place_screen.dart';
 
 class BottomNavBar extends StatefulWidget {
   const BottomNavBar({super.key});
@@ -17,8 +14,8 @@ class BottomNavBar extends StatefulWidget {
 }
 
 class _BottomNavBarState extends State<BottomNavBar> {
-  final _notchBottomBarController = NotchBottomBarController();
   final PageController _pageController = PageController();
+  int _currentIndex = 0; // Keep track of the selected tab
 
   final inActiveIconList = <String>[
     'assets/icons/bottomNavbaricons/InactiveIcons/home.svg',
@@ -26,12 +23,14 @@ class _BottomNavBarState extends State<BottomNavBar> {
     'assets/icons/bottomNavbaricons/InactiveIcons/workout.svg',
     'assets/icons/bottomNavbaricons/InactiveIcons/settings.svg',
   ];
+
   final activeIconList = <String>[
     'assets/icons/bottomNavbaricons/activeIcons/home.svg',
     'assets/icons/bottomNavbaricons/activeIcons/shop.svg',
     'assets/icons/bottomNavbaricons/activeIcons/workout.svg',
     'assets/icons/bottomNavbaricons/activeIcons/settings.svg',
   ];
+
   final itemLabel = <String>[
     'Home',
     'Shop',
@@ -47,59 +46,67 @@ class _BottomNavBarState extends State<BottomNavBar> {
           PageView(
             controller: _pageController,
             onPageChanged: (index) {
-              _notchBottomBarController
-                  .jumpTo(index); // Sync page index with NotchBottomBar
+              setState(() {
+                _currentIndex = index;
+              });
             },
             children: const [
               HomePage(),
               MarketplaceScreen(),
               Center(child: Text('Workout Tracking')),
-              Center(child: Text('Settings page')),
+              Center(child: Text('Settings Page')),
             ],
           ),
-          Positioned(
-            bottom: 0,
-            child: AnimatedNotchBottomBar(
+          // CurvedNavigationBar
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: CurvedNavigationBar(
+              index: _currentIndex,
+              height: 70.0,
+              backgroundColor: Colors.transparent, // Transparent behind the curve
               color: Colors.black,
-              notchBottomBarController: _notchBottomBarController,
-              bottomBarItems: List.generate(inActiveIconList.length, (index) {
-                return BottomBarItem(
-                  inActiveItem: SvgPicture.asset(
-                    inActiveIconList[index],
-                    colorFilter:
-                        const ColorFilter.mode(Colors.white, BlendMode.srcIn),
-                    clipBehavior: Clip.antiAliasWithSaveLayer,
-                  ),
-                  activeItem: SvgPicture.asset(
-                    clipBehavior: Clip.hardEdge,
+              buttonBackgroundColor: TColors.primary, // The primary color
+              items: List.generate(inActiveIconList.length, (index) {
+                return _currentIndex == index?Padding(
+                  padding: const EdgeInsets.all(5.0),
+                  child: SvgPicture.asset(
                     activeIconList[index],
-                    colorFilter: const ColorFilter.mode(
-                        TColors.primary, BlendMode.srcIn),
-                    // Active color of the icon
+                    width: 25,
+                    height: 25,
+                    colorFilter: ColorFilter.mode(
+                       Colors.white,
+                      BlendMode.srcIn,
+                    ),
                   ),
-                  itemLabel: itemLabel[index], // Label for each item
+                ):
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    SvgPicture.asset(
+                      inActiveIconList[index],
+                      width: 25,
+                      height: 25,
+                      colorFilter: ColorFilter.mode(
+                        Colors.white,
+                        BlendMode.srcIn,
+                      ),
+                    ),
+                    Text(itemLabel[index],style: TextStyle(color: Colors.white),)
+                  ],
                 );
               }),
               onTap: (index) {
+                setState(() {
+                  _currentIndex = index;
+                });
                 _pageController.animateToPage(
                   index,
                   duration: const Duration(milliseconds: 300),
-                  curve: Curves.bounceIn,
+                  curve: Curves.easeInOut,
                 );
               },
-              removeMargins: false,
-              bottomBarHeight: 80,
-              bottomBarWidth: MediaQuery.of(context).size.width,
-              durationInMilliSeconds: 50,
-              notchColor: Colors.white,
-              showLabel: true,
-              itemLabelStyle: const TextStyle(
-                color: Colors.white,
-                fontSize: 10,
-              ),
-              //elevation: 100.0,
-              kIconSize: 25,
-              kBottomRadius: 30,
+              animationCurve: Curves.easeInOut,
+              animationDuration: const Duration(milliseconds: 300),
             ),
           ),
         ],
